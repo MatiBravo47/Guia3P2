2ebde580eb763d99112665c47c2835048c9752a5
MatiBravo47 <matibravoneron@gmail.com>
2024-05-19 21:55:38 -0300

    Agrego ejercicios

--- Guia3P2/Program.cs
+++ Guia3P2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -65,6 +66,7 @@ namespace Guia3P2
              * Producto esperado: Un programa que solicita al usuario un numero del 1 al 7 
              * y muestra el dia de la semana correspondiente.
             */
+            /*
             int numeroDia;
             Console.WriteLine("Ingrese un numero del 1 al 7");
             numeroDia = int.Parse(Console.ReadLine());
@@ -97,6 +99,139 @@ namespace Guia3P2
                     break;
             }
             Console.ReadLine();
+            */
+            /*
+             * Conversion de califaciones a letras:
+             * Objetivo: Escribir un programa que tome una calificacion numerica
+             * y la convierta a una letra segun una escala predefinida.
+             * Producto esperado: Un programa que solicita al usuario una calificacion 
+             * numerica y muestra la calificacion en letra.
+            */
+            /*
+            int nota1;
+            Console.WriteLine("Ingrese calificacion");
+            nota1 = int.Parse(Console.ReadLine());
+            
+            switch (nota1)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    Console.WriteLine("D");
+                    break;
+                case 4:
+                case 5:
+                case 6:
+                    Console.WriteLine("C");
+                    break;
+                case 7:
+                case 8:
+                    Console.WriteLine("B");
+                    break;
+                case 9:
+                case 10:
+                    Console.WriteLine("A");
+                    break;
+                default:
+                    Console.WriteLine("Ingresaste una calificacion incorrecta");
+                    break;
+            }
+        Console.ReadLine();
+        */
+            /*
+             *Clasificacion de triangulos
+             *Objetivo: Crear un programa que determine si un triangulo es equilatero,
+             *isosceles o escaleno, dados los valores de sus tres lados.
+            */
+            /*
+             * Estaciones del anio:
+             * Objetivos: Desarrollar un programa que, dado un numero de mes
+             * identifique la estacion del anio correspondiente.
+             * Producto esperado: Un programa que solicita al usuario un numero 
+             * de mes y muestra la estacion del anio correspondiente.
+            */
+            /*
+            int numeroMes;
+            Console.WriteLine("Ingrese un mes");
+            numeroMes = int.Parse(Console.ReadLine());
+
+            switch (numeroMes)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    Console.WriteLine("Verano");
+                    break;
+                case 4: 
+                case 5:
+                case 6:
+                    Console.WriteLine("Otonio");
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                    Console.WriteLine("Invierno");
+                    break;
+                case 10:
+                case 11:
+                case 12:
+                    Console.WriteLine("Primavera");
+                    break;
+            }
+            Console.ReadLine();
+        */
+            /*
+             * Dias en un mes:
+             * Objetivo: Escribir un programa que, dado un numero de mes, determine
+             * la cantidad de dias que tiene ese mes.
+             * Producto esperado: Un programa que solicita al usuario un numero de mes
+             * y muestra la cantidad de dias que tiene ese mes.
+            */
+            int numeroMes;
+            Console.WriteLine("Ingrese numero de mes");
+            numeroMes = int.Parse(Console.ReadLine());
+
+            switch (numeroMes)
+            {
+                case 1:
+                    Console.WriteLine("31");
+                    break;
+                case 2:
+                    Console.WriteLine("28");
+                    break;
+                case 3:
+                    Console.WriteLine("31");
+                    break;
+                case 4:
+                    Console.WriteLine("30");
+                    break;
+                case 5:
+                    Console.WriteLine("31");
+                    break;
+                case 6:
+                    Console.WriteLine("30");
+                    break;
+                case 7:
+                case 8:
+                    Console.WriteLine("31");
+                    break;
+                case 9:
+                    Console.WriteLine("30");
+                    break;
+                case 10:
+                    Console.WriteLine("31");
+                    break;
+                case 11:
+                    Console.WriteLine("30");
+                    break;
+                case 12:
+                    Console.WriteLine("31");
+                    break;
+                default:
+                    Console.WriteLine("Numero de mes incorrecto");
+                    break;
+            }
+        Console.ReadLine();
         }
 
     }
